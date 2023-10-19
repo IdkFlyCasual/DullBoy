ff9f09a7f5f868c46406cba892dcc327714cf4dc
IdkFlyCasual <SuperValidEmailAddress@gmail.com>
2023-10-19 13:08:07 -0700

    first commit

---
+++ dullboy.py
@@ -0,0 +1,10 @@
+
+
+def define_base(int):
+    base = 'all work and no play makes jack a dull boy.'
+    arr = []
+    for i in range(0, int):
+        arr.append(base.split(' '))
+    print(arr)
+
+define_base(20)
